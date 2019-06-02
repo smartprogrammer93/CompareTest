2bedfa16b18a92af38fa18a1c2e8ddc69cedb18c
Ahmad Bitar <smartprogrammer@windowslive.com>
2019-06-02 09:30:38 +0300

    Init

---
+++ CompareBlazor/CompareBlazor/ResetPasswordModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompareBlazor
+{
+    public class ResetPasswordModel
+    {
+
+        [Required]
+        [Display(Name = "New Password")]
+        [StringLength(20, MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+        
+        [Required]
+        [Display(Name = "Confirm New Password")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(NewPassword),
+            ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmNewPassword { get; set; }
+
+    }
+}
